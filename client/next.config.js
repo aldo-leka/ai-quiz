65b2606601770ceed119ecd026d3fd5b5ff73a88
Aldo Leka <aldo.leka@live.com>
2025-04-24 17:28:07 +0200

    vercel failed deployment because of this??

--- client/next.config.js
+++ client/next.config.js
@@ -14,8 +14,8 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === 'production',
+  // },
 };
 
